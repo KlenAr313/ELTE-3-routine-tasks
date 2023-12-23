f2dd36329ceda425eadfc7f92f3d3d6d55fd4f83
Klenovszky Áron <aron.klenovszky@gmail.com>
2023-12-23 22:01:31 +0100

    Progress ¯\_(ツ)_/¯

--- Minefield/MInefield.MAUI/App.xaml.cs
+++ Minefield/MInefield.MAUI/App.xaml.cs
@@ -1,19 +1,78 @@
-﻿using Minefield.MAUI.ViewModel;
+﻿using Minefield.MAUI.Persistence;
+using Minefield.MAUI.ViewModel;
 using Minefield.Model;
+using Minefield.Persistence;
 
 namespace Minefield.MAUI
 {
     public partial class App : Application, IDisposable
     {
-        private MinefieldGameModel gameModel = null!;
-        private MinefieldViewModel viewModel = null!;
-        private IDispatcherTimer frameTick = null!;
+        private const string SuspendedGameSavePath = "SuspendedGame";
+
+        private readonly AppShell shell;
+        private MinefieldGameModel gameModel;
+        private readonly IStore store;
+        private readonly MinefieldViewModel viewModel;
 
         public App()
         {
             InitializeComponent();
 
-            MainPage = new AppShell(viewModel, gameModel);
+            store = new MinefieldStore();
+
+            gameModel = new MinefieldGameModel((int)DeviceDisplay.Current.MainDisplayInfo.Width, (int)DeviceDisplay.Current.MainDisplayInfo.Height);
+            viewModel = new MinefieldViewModel(gameModel);
+
+            shell = new AppShell(viewModel, gameModel, store)
+            {
+                BindingContext = viewModel
+            };
+
+            MainPage = shell;
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            Window window = base.CreateWindow(activationState);
+
+            window.Created += (s, e) =>
+            {
+                gameModel.StartGame();
+                shell.StartTimer();
+            };
+
+            window.Activated += (s, e) =>
+            {
+                if (!File.Exists(Path.Combine(FileSystem.AppDataDirectory, SuspendedGameSavePath)))
+                {
+                    return;
+                }
+
+                Task.Run(() =>
+                {
+                    try
+                    {
+                        gameModel = new MinefieldGameModel((int)DeviceDisplay.Current.MainDisplayInfo.Width, (int)DeviceDisplay.Current.MainDisplayInfo.Height, new DataAccess(Path.Combine(FileSystem.AppDataDirectory, SuspendedGameSavePath)));
+
+                        viewModel.NewModel(gameModel);
+
+                        gameModel.StartGame();
+                        shell.StartTimer();
+                    }
+                    catch { }
+                });
+            };
+
+            window.Deactivated += (s, e) =>
+            {
+                Task.Run(() =>
+                {
+                    shell.StopTimer();
+                    gameModel.SaveGame(new DataAccess(Path.Combine(FileSystem.AppDataDirectory, SuspendedGameSavePath)));
+                });
+            };
+
+            return window;
         }
 
 
@@ -22,6 +81,7 @@ namespace Minefield.MAUI
         /// </summary>
         public void Dispose()
         {
+            shell.Dispose();
             gameModel.Dispose();
             viewModel.Dispose();
         }
--- Minefield/MInefield.MAUI/AppShell.xaml.cs
+++ Minefield/MInefield.MAUI/AppShell.xaml.cs
@@ -8,13 +8,119 @@ namespace Minefield.MAUI
     {
         private MinefieldGameModel gameModel = null!;
         private MinefieldViewModel viewModel = null!;
+
         private IDispatcherTimer frameTick = null!;
 
-        public AppShell(MinefieldViewModel viewModel, MinefieldGameModel gameModel)
+        private readonly IStore store = null!;
+        private readonly StoredGameBrowserModel storedBrowserModel = null!;
+        private readonly StoredGameBorwserViewModel storedBrowserViewModel = null!;
+
+        public AppShell(MinefieldViewModel viewModel, MinefieldGameModel gameModel, IStore store)
         {
             InitializeComponent();
-            this.viewModel = viewModel;
+            this.store = store;
             this.gameModel = gameModel;
+            this.viewModel = viewModel;
+
+            frameTick = Dispatcher.CreateTimer();
+            frameTick.Interval = TimeSpan.FromMilliseconds(20);
+            frameTick.Tick += (_, _) => Frame();
+
+            gameModel.End += GameModel_End;
+
+            viewModel.NewGame += ViewModel_NewGame;
+            viewModel.LoadGame += ViewModel_LoadGame;
+            viewModel.SaveGame += ViewModel_SaveGame;
+            viewModel.ExitGame += ViewModel_ExitGame;
+
+            storedBrowserModel = new StoredGameBrowserModel(store);
+            storedBrowserViewModel = new StoredGameBorwserViewModel(storedBrowserModel);
+            storedBrowserViewModel.GameLoading += BrowserViewModel_GameLoading;
+            storedBrowserViewModel.GameSaving += BrowserViewModel_GameSaving;
+
+        }
+
+        private void Frame()
+        {
+            gameModel.OnFrame();
+        }
+
+        internal void StartTimer()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void StopTimer()
+        {
+            throw new NotImplementedException();
+        }
+        
+
+
+        private void ViewModel_ExitGame(object? sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void ViewModel_SaveGame(object? sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void ViewModel_LoadGame(object? sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void ViewModel_NewGame(object? sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void GameModel_End(object? sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+        private void BrowserViewModel_GameSaving(object? sender, StoredGameEventArgs e)
+        {
+            Navigation.PopAsync();
+            StopTimer();
+
+            try
+            {
+                gameModel.SaveGame(new DataAccess(Path.Combine(FileSystem.AppDataDirectory, e.Name)));
+                DisplayAlert("Mindefield", "Successful save", "Ok");
+            }
+            catch
+            {
+
+                DisplayAlert("Mindefield", "Unsuccessful save", "Ok");
+            }
+        }
+
+        private void BrowserViewModel_GameLoading(object? sender, StoredGameEventArgs e)
+        {
+            Navigation.PopAsync();
+
+            try
+            {
+                gameModel = new MinefieldGameModel((int)DeviceDisplay.Current.MainDisplayInfo.Width, (int)DeviceDisplay.Current.MainDisplayInfo.Height,
+                    new DataAccess(Path.Combine(FileSystem.AppDataDirectory, e.Name)));
+                viewModel.NewModel(gameModel);
+                
+                Navigation.PopAsync();
+                DisplayAlert("Mindefield", "Successful loading", "Ok");
+
+                gameModel.StartGame();
+                StartTimer();
+            }
+            catch
+            {
+                DisplayAlert("Mindefield", "Unsuccessful loading", "Ok");
+            }
         }
 
 
